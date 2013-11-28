9b7319dc4317c2bc88374e3f59c5c2f008326982
WalrusCow <willgmcd@gmail.com>
2013-11-27 21:23:09 -0600

    Work in progress...

---
+++ page.js
@@ -0,0 +1,74 @@
+
+
+function Page(id, requestQueue, pollFunction) {
+  this.id = id;
+  this.requestQueue = requestQueue;
+  this.pollFunction = pollFunction;
+}
+
+Page.prototype.setFn = function(name, fn, cb) {
+  var args = [this.id, 'setFunction', name, fn.toString()];
+  this.requestQueue.push([args, makeSafeCallback(cb, this.pollFunction)]);
+};
+
+Page.prototype.get = function(name, cb) {
+  var args = [this.id, 'getProperty', name];
+  this.requestQueue.push([args, makeSafeCallback(cb, this.pollFunction)]);
+};
+
+Page.prototype.set = function(name, val, cb) {
+  var args = [this.id, 'setProperty', name, val];
+  this.requestQueue.push([args, makeSafeCallback(cb, this.pollFunction)]);
+};
+
+Page.prototype.evaluate = function(fn, cb) {
+  var args = [id, 'evaluate', fn.toString()];
+  if (arguments.length > 2) {
+    args.concat(Array.prototype.slice.call(arguments, 2));
+  }
+  this.requestQueue.push([args, makeSafeCallback(cb, this.pollFunction)]);
+};
+
+Page.prototype.waitForSelector = function(selector, cb, timeout) {
+  /*
+   * Wait for an element matching the selector to appear. Wait a maximum
+   * (approximately) of timeout milliseconds.
+   */
+
+  // Default timeout of 10 seconds
+  timeout = timeout || 10000;
+
+  // We need to not wait more than this long
+  var startTime = Date.now();
+
+  // Time between selector checks
+  var timeoutInterval = 150;
+
+  function testForSelector() {
+    // How long it has been since we started this function
+    var elapsedTime = Date.now() - startTime;
+
+    // If true, we have timed out
+    if (elapsedTime > timeout) {
+      return cb('Timeout waiting for selector: ' + selector);
+    }
+
+    // TODO: wtf... why this... ?
+    // Evaluate the selector
+    this.evaluate(function(selector) {
+      return document.querySelectorAll(selector).length;
+    }, function(result) {
+      // We found the selector
+      if (result > 0) {
+        cb();
+      }
+      else {
+        // Retry the test
+        setTimeout(testForSelector, timeoutInterval);
+      }
+    }, selector);
+  }
+
+  // Begin testing for the selector
+  setTimeout(testForSelector, timeoutInterval);
+};
